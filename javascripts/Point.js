52ba2e8cb4b1cca0a174fb5151ff4686abe31f55
Daniel Schlaug <daniel.schlaug@gmail.com>
2012-10-12 15:54:54 +0200

    Added color support in Point

--- javascripts/Point.js
+++ javascripts/Point.js
@@ -6,12 +6,14 @@
  * @param x   x position or a Point object from which to copy coordinates
  * @param y   y position
  */
-function Point(x, y){
+function Point(x, y, color){
 	if (x instanceof Point) {
 		this.x = x.x;
 		this.y = x.y;
+		this.color = x.color;
 	} else {
 	    this.x = x;
 	    this.y = y;
+	    this.color = color;
 	}
 }
